ac062804d00e852b91eb274aa280a5d63098a9f4
Azkarell <schroder_daniel@gmx.net>
2025-12-23 17:04:00 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,11 @@ pub trait GlobalObserverRegistration: Send + Sync {
     fn register_observer(&self, app: &mut App);
 }
 
-pub struct FunctionObserverRegistration<'a, F: Send + Sync, Marker, E: Event, B: Bundle>
+pub struct FunctionObserverRegistration<F: Send + Sync, Marker, E: Event, B: Bundle>
 where
-    &'a F: IntoObserverSystem<E, B, Marker>,
+    F: IntoObserverSystem<E, B, Marker> + Copy,
 {
-    f: &'a F,
+    f: F,
     _pd: PhantomData<dyn Fn((Marker, B, E)) + Send + Sync>,
 }
 pub trait IntoEntityObserverRegistration<R: EntityObserverRegistration> {
@@ -31,13 +31,13 @@ pub trait IntoGlobalObserverRegistration<R: GlobalObserverRegistration> {
     fn into_registration(self) -> R;
 }
 
-impl<'a, F: Send + Sync, E: EntityEvent, B: Bundle, Marker>
-    IntoEntityObserverRegistration<FunctionObserverRegistration<'a, F, Marker, E, B>> for &'a F
+impl<F: Send + Sync, E: EntityEvent, B: Bundle, Marker>
+    IntoEntityObserverRegistration<FunctionObserverRegistration<F, Marker, E, B>> for F
 where
-    FunctionObserverRegistration<'a, F, Marker, E, B>: EntityObserverRegistration,
-    &'a F: IntoObserverSystem<E, B, Marker>,
+    FunctionObserverRegistration<F, Marker, E, B>: EntityObserverRegistration,
+    F: IntoObserverSystem<E, B, Marker> + Copy,
 {
-    fn into_registration(self) -> FunctionObserverRegistration<'a, F, Marker, E, B> {
+    fn into_registration(self) -> FunctionObserverRegistration<F, Marker, E, B> {
         FunctionObserverRegistration {
             f: self,
             _pd: Default::default(),
@@ -45,23 +45,22 @@ where
     }
 }
 
-impl<'a, F: Send + Sync, Marker, E: Event, B: Bundle> GlobalObserverRegistration
-    for FunctionObserverRegistration<'a, F, Marker, E, B>
+impl<F: Send + Sync, Marker, E: Event, B: Bundle> GlobalObserverRegistration
+    for FunctionObserverRegistration<F, Marker, E, B>
 where
-    &'a F: IntoObserverSystem<E, B, Marker>,
+    F: IntoObserverSystem<E, B, Marker> + Copy,
 {
     fn register_observer(&self, app: &mut App) {
         app.add_observer(self.f);
     }
 }
 
-impl<'a, F: Send + Sync, Marker, E: EntityEvent, B: Bundle> EntityObserverRegistration
-    for FunctionObserverRegistration<'a, F, Marker, E, B>
+impl<F: Send + Sync, Marker, E: EntityEvent, B: Bundle> EntityObserverRegistration
+    for FunctionObserverRegistration<F, Marker, E, B>
 where
-    &'a F: IntoObserverSystem<E, B, Marker>,
+    F: IntoObserverSystem<E, B, Marker> + Copy,
 {
     fn register_observer(&self, app: &mut EntityCommands) {
         app.observe(self.f);
     }
 }
-
